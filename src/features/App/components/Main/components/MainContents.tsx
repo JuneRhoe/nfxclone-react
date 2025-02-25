a5521c333ad17dfd46cc6a35d3bb3d6967fd29d8
JuneRhoe <junghyun.rhoe@gmail.com>
2025-02-25 14:14:55 -0500

    Update test for deployment testing

--- src/features/App/components/Main/components/MainContents.tsx
+++ src/features/App/components/Main/components/MainContents.tsx
@@ -1,7 +1,7 @@
 export default function MainContents() {
   return (
     <div className="flex h-full w-full items-center justify-center text-5xl">
-      Main Content
+      ===== Main Content Deployed =====
     </div>
   )
 }
