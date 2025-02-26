2ca33937eacf475db454415afba5f1a939371a0b
Jung Hyun Rhoe <31903631+JuneRhoe@users.noreply.github.com>
2025-02-26 16:52:22 -0500

    Endpoint Configuration (#5) (#6)
    * Config an env value for the endpoint
    
    * Rename components to submodule
    
    * Initialize TanStack (React Query)
    
    * Add a package for cookies
    
    * Add a package for handling a form
    
    * Add query and mutation hooks and utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  server: {
-    port: 3333 // change here
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      checker({
+        typescript: true,
+      }),
+    ],
+    server: {
+      port: 3333 // change here
     },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      },
+    },
+    define: {
+      'process.env': {...process.env, ...loadEnv(mode, process.cwd(), '')}
+    }
   }
 })
