0be6963d073d5e1bed5623c524af772b30fc3b34
JuneRhoe <junghyun.rhoe@gmail.com>
2025-02-26 15:13:05 -0500

    Add query and mutation hooks and utils

---
+++ src/submodule/tanstack/hooks.ts
@@ -0,0 +1,28 @@
+import { DefaultError, DefinedUseQueryResult, MutationFunction, QueryFunction, QueryKey, SkipToken, UseInfiniteQueryOptions, useMutation, useQuery, UseQueryOptions, UseQueryResult } from "@tanstack/react-query";
+
+export function useTackstackQuery<TQueryFnData, TError = DefaultError, TData = TQueryFnData, TQueryKey extends QueryKey = QueryKey, TPageParam = unknown>(
+  queryKey:  TQueryKey & {},
+  queryFn: QueryFunction<TQueryFnData, TQueryKey> |
+    Exclude<UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>['queryFn'], SkipToken> |
+    Exclude<UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey, TPageParam>['queryFn'], SkipToken>
+  
+): DefinedUseQueryResult<TData, TError> | UseQueryResult<TData, TError> | UseQueryResult<TData, TError> {
+   return useQuery<TQueryFnData, TError, TData, TQueryKey>({ queryKey, queryFn });
+}
+
+export function useTanstackMutation<TData, TError = DefaultError, TVariables = void, TContext = unknown>(
+  mutationFn: MutationFunction<TData, TVariables>,
+  onSettled: (data: TData | undefined, error: TError | null, variables: TVariables, context: TContext | undefined) =>
+    Promise<unknown> | unknown,
+  onMutate?: (variables: TVariables) => Promise<TContext | undefined> | TContext | undefined,
+  onSuccess?: (data: TData, variables: TVariables, context: TContext) => Promise<unknown> | unknown,
+  onError?: (error: TError, variables: TVariables, context: TContext | undefined) => Promise<unknown> | unknown
+) {
+  return useMutation({
+    mutationFn,    
+    onMutate,
+    onSuccess,
+    onError,
+    onSettled,
+  })
+}
--- src/submodule/tanstack/utils.ts
+++ src/submodule/tanstack/utils.ts
@@ -1,5 +1,42 @@
-export async function getQueryFunction(endPoint: string) {
-  const response = await fetch(`https://${process.env.API_KEY}.mockapi.io/api/${endPoint}`)
+const API_URL = `https://${process.env.API_KEY}.mockapi.io/api/`
+
+export async function queryFunction<TQueryFnData>(endPoint: string): Promise<TQueryFnData> {
+  let response: Response | null = null
+
+  try {
+    response = await fetch(`${API_URL}${endPoint}`, {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    })
+    
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+  } catch (e) {
+    // Error handling
+    console.error(e)
+  }
   
-  return await response.json();
+  return await response?.json();
+}
+
+export async function mutationFunction<TData>(endPoint: string, newData: TData): Promise<TData> {
+  let response: Response | null = null
+
+  try {
+    response = await fetch(`${API_URL}${endPoint}`, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newData)
+    })
+
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+  } catch (e) {
+    // Error handling
+    console.error(e)
+  }  
+
+  return await response?.json();
 }
