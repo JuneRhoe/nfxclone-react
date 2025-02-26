1135e77d095ca888a3ab7cfcb5510b259f2a29d1
Jung Hyun Rhoe <31903631+JuneRhoe@users.noreply.github.com>
2025-02-26 16:55:49 -0500

    Product Release (#7)
    * Endpoint Configuration (#5)
    
    * Config an env value for the endpoint
    
    * Rename components to submodule
    
    * Initialize TanStack (React Query)
    
    * Add a package for cookies
    
    * Add a package for handling a form
    
    * Add query and mutation hooks and utils
    
    * Endpoint Configuration (#5) (#6)
    
    * Config an env value for the endpoint
    
    * Rename components to submodule
    
    * Initialize TanStack (React Query)
    
    * Add a package for cookies
    
    * Add a package for handling a form
    
    * Add query and mutation hooks and utils

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import '@/styles/index.css'
 import App from '@/features/App/App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { queryClient } from './submodule/tanstack/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
