74d714631025fd61ba26bd18650bccb351c2a5cc
Jung Hyun Rhoe <31903631+JuneRhoe@users.noreply.github.com>
2025-02-27 16:24:21 -0500

    Update routes and components (#8)

--- src/features/App/components/Main/components/MainContents.tsx
+++
@@ -1,7 +0,0 @@
-export default function MainContents() {
-  return (
-    <div className="flex h-full w-full items-center justify-center text-5xl">
-      ===== Main Content Deployed =====
-    </div>
-  )
-}
---
+++ src/features/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react'
+import { ThemeInfoContext } from '../App/context'
+import clsx from 'clsx'
+
+export default function Footer() {
+  const { themeType } = useContext(ThemeInfoContext)
+
+  return (
+    <div
+      className={clsx('flex h-[10rem] w-full items-center justify-center', {
+        'text-[#737373]': themeType === 'lightMode',
+        'text-white': themeType === 'darkMode',
+      })}
+    >
+      Footer
+    </div>
+  )
+}
