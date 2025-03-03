c4de0d05e954a057777040f02d9b1aa08c8ce846
JuneRhoe <junghyun.rhoe@gmail.com>
2025-03-02 20:07:44 -0500

    Update the shared UI components

--- src/submodule/components/Loader/Loader.tsx
+++ src/submodule/components/Loader/Loader.tsx
@@ -1,7 +1,31 @@
-export default function Loader() {
+import clsx from 'clsx'
+
+type LoaderType = 'primary' | 'secondary'
+type DisplayType = 'full' | 'inline'
+
+interface Props {
+  type?: LoaderType
+  display?: DisplayType
+}
+
+export default function Loader({ type = 'primary', display = 'full' }: Props) {
   return (
-    <div className="flex h-full min-h-[100vh] w-full items-center justify-center">
-      <div className="h-30 w-30 animate-spin rounded-[50%] border-10 border-gray-600 border-b-transparent" />
+    <div
+      className={clsx('flex items-center justify-center', {
+        'bg-[#202020]': type === 'primary',
+        'bg-white': type === 'secondary',
+        'w-full h-full min-h-[100vh]': display === 'full',
+        'w-full h-full bg-transparent': display === 'inline',
+      })}
+    >
+      <div
+        className={clsx('animate-spin rounded-[50%] border-b-transparent', {
+          'border-gray-100': type === 'primary',
+          'border-gray-500': type === 'secondary',
+          'border-10 h-20 w-20': display === 'full',
+          'border-3 h-6 w-6': display === 'inline',
+        })}
+      />
     </div>
   )
 }
