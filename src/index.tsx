b3eef64087c86beb94f76226f2e26a2a79d4398f
JuneRhoe <junghyun.rhoe@gmail.com>
2025-03-02 20:15:00 -0500

    Add a cookie provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import '@/styles/index.css'
-import App from '@/features/App/App.tsx'
+import { CookiesProvider } from 'react-cookie'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import '@/styles/index.css'
+import App from '@/features/App/App.tsx'
 import { queryClient } from './submodule/tanstack/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </CookiesProvider>
   </StrictMode>,
 )
