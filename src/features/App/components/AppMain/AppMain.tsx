f1ff72ad6d96ad58923d98b28a6c76433169f6e0
JuneRhoe <junghyun.rhoe@gmail.com>
2025-03-06 15:03:55 -0500

    Add scrollTop to AppMain

--- src/features/App/components/AppMain/AppMain.tsx
+++ src/features/App/components/AppMain/AppMain.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/features/Footer/Footer'
-import bgHome from '@/assets/image/home/bg-home.jpg'
+import bgHome from '@/assets/images/home/bg-home.jpg'
 import AppTopNavBar from './components/AppTopNavBar'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
@@ -9,6 +9,7 @@ import { useContext } from 'react'
 import { ThemeInfoContext } from '../../context'
 import { useCheckUserInfo } from './hooks'
 import { browsePath } from '@/routes'
+import ScrollToTop from '@/submodule/components/ScrollToTop/ScrollToTop'
 
 export type DisplayType = 'signIn' | 'signUp' | 'register'
 
@@ -39,35 +40,41 @@ export default function AppMain({ displayType }: AppMainProps) {
   }
 
   return (
-    <div
-      className={clsx(
-        'flex h-full min-h-[100vh] w-full flex-col items-start justify-start',
-        {
-          'bg-[#202020]': themeType === 'darkMode',
-          'bg-[#F3F3F3]': themeType === 'lightMode',
-        },
-      )}
-    >
+    <>
+      <ScrollToTop />
       <div
-        className={`relative flex h-[85vh] min-h-[37.5rem] w-full flex-col items-center
-          sm:min-h-[45rem]`}
+        className={clsx(
+          'flex h-full min-h-[100vh] w-full flex-col items-start justify-start',
+          {
+            'bg-[#101010]': themeType === 'darkMode',
+            'bg-[#F3F3F3]': themeType === 'lightMode',
+          },
+        )}
       >
         <div
-          className={'absolute h-full w-full bg-cover bg-center opacity-50'}
-          style={{ backgroundImage: `url(${bgHome})` }}
-        />
-        <div className="absolute flex h-full w-full flex-col items-center justify-center">
-          {mainContentFC}
-        </div>
-        <div
-          className={clsx('relative flex h-fit w-full flex-col items-center', {
-            'xl:w-[80%] 2xl:w-[70%]': themeType === 'darkMode',
-          })}
+          className={`relative flex h-[85vh] min-h-[37.5rem] w-full flex-col items-center
+            sm:min-h-[45rem]`}
         >
-          <AppTopNavBar displayType={displayType} />
+          <div
+            className={'absolute h-full w-full bg-cover bg-center opacity-50'}
+            style={{ backgroundImage: `url(${bgHome})` }}
+          />
+          <div className="absolute flex h-full w-full flex-col items-center justify-center">
+            {mainContentFC}
+          </div>
+          <div
+            className={clsx(
+              'relative flex h-fit w-full flex-col items-center',
+              {
+                'xl:w-[80%] 2xl:w-[70%]': themeType === 'darkMode',
+              },
+            )}
+          >
+            <AppTopNavBar displayType={displayType} />
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
