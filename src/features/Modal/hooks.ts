053ee5e269fbac8f44348ea3ef65095779f8d21b
JuneRhoe <junghyun.rhoe@gmail.com>
2025-03-15 16:45:47 -0400

    Update modal related components

---
+++ src/features/Modal/hooks.ts
@@ -0,0 +1,10 @@
+import { useAppDispatch, useAppSelector } from "@/features/store/hooks";
+import { ModalState, selectModalInfoArray } from "@/submodule/components/Modal/modalSlice";
+import { ModalCreationInfo, ModalInfo, useModalWrapper } from "@/submodule/components/Modal/hooks";
+
+export function useModal(modalInfo?: ModalInfo): ModalCreationInfo {
+  const dispatch = useAppDispatch()
+  const modalInfoArray = useAppSelector(selectModalInfoArray)
+
+  return useModalWrapper<{ modalState: ModalState }>(dispatch, modalInfoArray, modalInfo)
+}
