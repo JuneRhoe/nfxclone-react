897c7e4fcbe944b831d6270152aaa056963cd3aa
JuneRhoe <junghyun.rhoe@gmail.com>
2025-03-19 14:40:08 -0400

    Update my information

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -1,28 +1,20 @@
-import { Link } from 'react-router'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import clsx from 'clsx'
 import { useAppSelector } from '../store/hooks'
 import { selectThemeMode } from '../store/themeSlice'
+import MyInfo from '../MyInfo/MyInfo'
 
 export default function Footer() {
   const themeMode = useAppSelector(selectThemeMode)
 
   return (
     <div
-      className={clsx('flex h-[7.5rem] w-full items-center justify-center', {
+      className={clsx('flex items-center justify-center h-[7.5rem] w-full', {
         'text-[#737373]': themeMode === 'lightMode',
         'text-white': themeMode === 'darkMode',
       })}
     >
-      <div className="flex items-center gap-2 text-base sm:text-2xl">
-        <div>
-          <span className="text-gray-500">Built by</span> Jung Hyun Rhoe
-        </div>
-
-        <Link to="https://www.linkedin.com/in/junghyunrhoe/" target="_blank">
-          <FontAwesomeIcon icon={faLinkedin} fixedWidth />
-        </Link>
+      <div className="flex flex-col gap-2 items-center text-xl sm:text-2xl">
+        <MyInfo />
       </div>
     </div>
   )
