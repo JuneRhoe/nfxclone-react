adf48a18bb4d18724ea60bcc05336724873acda8
JuneRhoe <junghyun.rhoe@gmail.com>
2025-03-24 00:27:41 -0400

    Add missing cookie path

--- src/features/hooks.ts
+++ src/features/hooks.ts
@@ -7,8 +7,8 @@ export function useUserCookie() {
   return {
     storedUserId: cookies.userId?.trim(),
     storedPassword: cookies.userPassword?.trim(),
-    setUserIdCookie: (userId: string) => setCookies('userId', userId, { maxAge: 60 * 60 * 12 }),
-    setAuthTokenCookie: (authToken: string) => setCookies('authToken', authToken, { maxAge: 60 * 60 * 12 }),
+    setUserIdCookie: (userId: string) => setCookies('userId', userId, { maxAge: 60 * 60 * 12, path: '/' }),
+    setAuthTokenCookie: (authToken: string) => setCookies('authToken', authToken, { maxAge: 60 * 60 * 12, path: '/' }),
     removeUserIdCookie: () => removeCookies('userId'),
     removeAuthTokenCookie: () => removeCookies('authToken')
   }
